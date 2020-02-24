657f41b20f679c4a7f69660dc211735d27c72509
terance98 <thomasterance98@gmail.com>
2020-02-24 09:39:13 +0530

    API call is working

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express")
 
 const app = express()
 
-app.get("/", (req,res) => res.send("Hello world"));
+//Use the lambda function name along with the '/' when performing GET/POST
+
+app.get("/lambda-express", (req,res) => res.send("Hello world"));
 
 module.exports = app
